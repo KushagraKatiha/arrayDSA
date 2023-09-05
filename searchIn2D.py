b228f12bb94e732bbf257ed18e65053584aad95f
KushagraKatiha <kushagrakatiha123@gmail.com>
2023-09-05 10:59:21 +0530

    binary search in 2D-matrix

---
+++ searchIn2D.py
@@ -0,0 +1,56 @@
+## Search for the given target in the 2D-array and if it is present in the array return True else False
+
+# Array
+arr = [[1,3,5,7], [10,11,16,20], [23,30,34,60]]
+
+# Target
+target = 7
+
+# No. of rows 
+r = len(arr)
+
+# No. of columns
+c = len(arr[0])
+
+####################### Brute Force Approach #############################
+
+def linearSearchIn2D(arr, target):
+    for i in range (r):
+        for j in range(c):
+            if arr[i][j] == target:
+                return True
+    return False
+
+## Function Call
+result = linearSearchIn2D(arr, target)
+print(result)
+
+
+############################# Binary Search Approach ###############################
+
+def binarySearchIn2D(arr, target):
+    # low & high of matrix
+    low , high = 0, (r*c)-1
+    if r == 0:
+        return False
+    else:
+        while low<=high:
+            # mid of the matrix
+            mid = low + (high - low) // 2
+
+            # mid element => arr[mid//c][mid%c]
+
+            if arr[mid//c][mid%c] == target:
+                return True
+            elif arr[mid//c][mid%c] > target:
+                high = mid-1
+            else: 
+                low = mid+1
+        
+    return False
+
+## Function call 
+result2 = binarySearchIn2D(arr, target)
+print(result2)
+    
+                 
