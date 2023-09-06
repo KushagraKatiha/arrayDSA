11f630a5a6d54e8ec597e4582ac8a891ca88a9dc
KushagraKatiha <kushagrakatiha123@gmail.com>
2023-09-06 10:30:03 +0530

    final commit

--- arr2.py
+++ arr2.py
@@ -18,16 +18,40 @@ def binarySearch(arr, lb, ub, x):
             return binarySearch(arr, lb, mid-1, x)
         elif arr[mid] < x:
             return binarySearch(arr, mid+1, ub, x)
+    return -1  
+
+## Ternary Search
+
+def ternarySearch(arr, x, lb, ub):
+    while lb<=ub:
+        mid1 = lb + (ub-lb) // 3       
+        mid2 = ub - (ub-lb) // 3
+
+        if arr[mid1] == x:
+            return mid1
+        elif arr[mid2] == x:
+            return mid2
+        elif arr[mid1] > x:
+            ub = mid1-1
+        elif arr[mid2] < x:
+            lb = mid2+1
+        else:
+            ub = mid2-1
+            lb = mid1+1
+
+    return -1            
 
 
 arr = [1,2,3,4,5,6,8]
-
+                                   
 lb = 0
 ub = len(arr)-1
-x = 5
+x = 4
 
 linearResult = linearSearch(arr, x)
 binaryResult = binarySearch(arr, lb, ub, x)
+ternaryResult = ternarySearch(arr, x, lb, ub)
 
 print(linearResult)
 print(binaryResult)
+print(ternaryResult)
