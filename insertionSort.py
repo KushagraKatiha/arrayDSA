7a369f3425566234e9cc6b598c6fccbf11569828
KushagraKatiha <kushagrakatiha123@gmail.com>
2023-09-08 17:19:40 +0530

    Insertion Sort

---
+++ insertionSort.py
@@ -0,0 +1,23 @@
+## Insertion Sort
+
+def insertionSort(arr):
+    for i in range(len(arr)):
+        key = arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+
+        arr[j+1] = key
+
+    return arr        
+
+
+
+
+
+## Driver Code
+arr = [9, 5, 1, 4, 3]
+
+result = insertionSort(arr)
+print(result)
